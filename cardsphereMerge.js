ac61da1916d0cd933375d596abc7a1745e2323fa
unknown <moreirazeantonio@gmail.com>
2020-02-07 16:51:35 +0000

    v1

--- cardsphereMerge.js
+++ cardsphereMerge.js
@@ -5,15 +5,14 @@ const mergeSameCards = ()=>{
   let repeated = [];
   //every card inside package
   let cards = [...elem.querySelectorAll('a.cardpeek')];
+  //it's ~O(n^2) but a small sample helps with this
   for(let i=0;i<cards.length;i++){
     let search = cards[i].innerText;
-    console.log(search,i);
     cards[i].dataset.total = 1;
     for(let f = i+1; f<cards.length;f++){
       //found repeated
       if(cards[f].innerText == search) { 
         let father = cards[f].parentNode.parentNode.parentNode;
-        console.log(cards[f].innerText);
         father.parentNode.removeChild(father);
         cards[i].dataset.total = parseInt(cards[i].dataset.total)+1;
       }
@@ -27,14 +26,15 @@ const mergeSameCards = ()=>{
 }
 
 const mergeButton = document.createElement('button');
-mergeButton.className ='btn btn-primary';
-mergeButton.innerText = 'Join Repeated Cards';
-mergeButton.style.position = 'fixed';
-mergeButton.style.bottom = '25px';
-mergeButton.style.right = '25px';
-mergeButton.style.zIndex = '10';
+mergeButton.className ='btn btn-default  btn-sm btn-primary';
+mergeButton.innerText = 'Merge Repeated Cards';
+//mergeButton.style.position = 'fixed';
+//mergeButton.style.bottom = '25px';
+//mergeButton.style.right = '25px';
+//mergeButton.style.zIndex = '10';
+mergeButton.style.marginLeft = '25px';
 mergeButton.addEventListener('click',()=>{mergeSameCards();});
-document.querySelector('body').appendChild(mergeButton);
+document.querySelector('.info-header .btn-group').appendChild(mergeButton);
    
 
   
